be23360fd40938fab5556ea54e6cad0f36f1af5b
user39043346 <143732806+user39043346@users.noreply.github.com>
2023-12-02 10:52:49 +0300

    edit l2

--- prog/l2/proj/src/main/java/pokemons/Hitmonlee.java
+++ prog/l2/proj/src/main/java/pokemons/Hitmonlee.java
@@ -7,11 +7,9 @@ import main.java.attacks.Swagger;
 
 import ru.ifmo.se.pokemon.*;
 
-public class Hitmonlee extends Pokemon {
+public class Hitmonlee extends Tyrogue {
     public Hitmonlee(String name, int lvl) {
         super(name, lvl);
-        this.setStats(50, 120, 53, 35, 110, 87);
-        this.setType(Type.FIGHTING);
-        this.setMove(new Swagger(), new Rest(), new BulkUp(), new Meditate());
+        this.addMove(new Meditate());
     }
 }
